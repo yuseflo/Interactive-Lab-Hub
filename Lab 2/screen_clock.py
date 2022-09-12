d11e6657cd3047aec29927bb2a149939241bd7e3
yuseflo <yi78@cornell.edu>
2022-09-12 12:49:51 -0400

    finish lab2-part1

--- Lab 2/screen_clock.py
+++ Lab 2/screen_clock.py
@@ -1,4 +1,6 @@
+from pickle import TRUE
 from time import strftime, sleep
+import time
 import subprocess
 import digitalio
 import board
@@ -64,19 +66,62 @@ backlight.value = True
 buttonA = digitalio.DigitalInOut(board.D23)
 buttonB = digitalio.DigitalInOut(board.D24)
 
-#disp.fill(screenColor)
-while True:
-    # Draw a black filled box to clear the image.
-    draw.rectangle((0, 0, width, height), outline=0, fill=0)
-    #disp.fill(screenColor)
-    #TODO: Lab 2 part D work should be filled in here. You should be able to look in cli_clock.py and stats.py
-    if buttonB.value and not buttonA.value:
-        #disp.fill(screenColor)
-        draw.text((x, 0), strftime("%H:%M:%S"), font = font, fill = "#FFFFFF")
-    if buttonA.value and not buttonB.value:
-        draw.text((x, 0), strftime("%H:%M:%S"), font = font, fill = "#FFFFFF")
-    
+# Initialize x-position of the 3 players
+x1 = 0
+x2 = 0
+x3 = 0
 
-    # Display image.
-    disp.image(image, rotation)
-    sleep(1)
+# Initialize time for countdown
+t = 0
+
+# Declare value to indicate if button is pressed once or not
+button_press = False
+
+while True:
+    
+    # Press button A
+    if not buttonA.value:
+        button_press = True
+    
+    if button_press:
+        # Draw a black filled box to clear the image.
+        draw.rectangle((0, 0, width, height), outline=0, fill=0)
+
+
+        world = "Olympics Running"
+
+        # Define countdown
+        mins, secs = divmod(t, 20)
+        timer = '{0}'.format(secs)
+        t -= 1
+
+        draw.text((0, 0), strftime("%a %d" ), font=font, fill="#F4E38E")
+        draw.text((75, 0), world, font=font, fill="#F4E38E")
+        draw.text((0, 110), "Countdown: {0} s".format(timer), font=font, fill="#FFFFF0")
+
+        # Define 3 players and their position
+        hash = "#"
+        draw.text((x1, 32), hash, font=font, fill="#0000FF")
+        ampersand = "&"
+        draw.text((x2, 55), dollar, font=font, fill="#00FF00")
+        dollar = "$"
+        draw.text((x3, 78), ampersand, font=font, fill="#FF0000")
+
+        x1 += 13.4
+        x2 += 16.8
+        x3 += 15.4
+
+        # Let players start again after they reach the border of the display
+        if x1 > 230:
+            x1 = 0
+
+        if x2 > 230:
+            x2 = 0
+
+        if x3 > 230:
+            x3 = 0 
+
+        # Display image.
+        disp.image(image, rotation)
+        time.sleep(1)
+
